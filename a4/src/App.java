310d21861c52fd4f49206f74f30b8b8d749a5dca
0katysha0 <kate.lozovaya230307@gmail.com>
2024-09-16 09:49:04 -0600

    cat and lama monday

---
+++ a4/src/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("       (\\         /)       ");
+        System.out.println("      :( > 0  .  0 <):      ");
+        System.out.println("         (``)--(``)         ");
+        System.out.println("                        ");
+        System.out.println("                        ");
+        System.out.println("                        ");
+        System.out.println("                        ");
+        System.out.println("      ^_______^   ");
+        System.out.println("     (0(' * ')0)           ");
+        System.out.println("       ( ___ )           ");
+        System.out.println("       (     )      ^     ");
+        System.out.println("       (       -----)           ");
+        System.out.println("       (            )           ");
+        System.out.println("        W||wwwwww||W        ");
+    }
+}
